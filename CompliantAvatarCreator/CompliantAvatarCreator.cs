d78e90bae00112ce327f92ea28c0fd2c90ef58a8
Sox <66733654+Sox-NeosVR@users.noreply.github.com>
2022-08-16 18:04:00 +0100

    e
    e

---
+++ CompliantAvatarCreator/CompliantAvatarCreator.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+
+namespace CompliantAvatarCreator
+{
+    public class CompliantAvatarCreator : NeosMod
+    {
+        public override string Name => "CompliantAvatarCreator";
+        public override string Author => "Sox";
+        public override string Version => "1.0.0";
+        public override string Link => "";
+
+        public object Config { get; private set; }
+
+        public override void OnEngineInit()
+        {
+
+            CompliantAvatarCreator.config = base.GetConfiguration();
+            Harmony harmony = new Harmony("net.Sox.CompliantAvatarCreator");
+            harmony.PatchAll();
+        }
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> JustDontDrive = new ModConfigurationKey<bool>("CompliantAvatarCreator", "Toggle 'DontDrive' on the Avatar Creator by default.", () => true, false, null);
+
+        private static ModConfiguration config;
+        [HarmonyPatch(typeof(AvatarCreator))]
+        class PatchAvatarCreator
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch("AddAnchor")]
+            public static void PostFix(Slider slider)
+            {
+                slider.DontDrive.Value = config.GetValue(JustDontDrive);
+            }
+        }   
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# CompliantAvatarCreator
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows you to stop that irritating snapping on when trying to set up an avatar in certain sessions (Especially when in a Headless session).
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [CompliantAvatarCreator.dll](https://github.com/Sox-NeosVR/CompliantAvatarCreator/releases/latest/download/CompliantAvatarCreator.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
